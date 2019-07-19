bdf7fd9f78fdd7724bcf94aef822c312bb04769a
Andreas Harasztosi <aph@gmx.at>
2019-07-19 18:09:43 +0200

    first commit

---
+++ actions/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'classes/Database.php';
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPW', '');
+define('DBNAME', 'cr11_andreasharasztosi_travelmatic');
+
+$trvomatic = new Database(DBHOST, DBUSER, DBPW, DBNAME);
